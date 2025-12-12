902c0d0e9732f6d67ba664da3ee5fd07ee07ebfd
Ed Jelizarov <eduards.jelizarovs@gmail.com>
2025-12-12 14:46:57 +0100

    ff

--- src/copy_content.py
+++ src/copy_content.py
@@ -3,21 +3,17 @@ import shutil
 from pathlib import Path
 from blocks import markdown_to_html_node
 
-TARGET_DIR = 'public'
-SOURCE_DIR = 'static'
-CONTENT_DIR = 'content'
-
-def copy_content():
-	for item in os.listdir(TARGET_DIR):
-		item_path = os.path.join(TARGET_DIR, item)
+def copy_content(source, target):
+	for item in os.listdir(target):
+		item_path = os.path.join(target, item)
 		if os.path.isfile(item_path):
 			os.unlink(item_path)
 		elif os.path.isdir(item_path):
 			shutil.rmtree(item_path)
 
-	for item in os.listdir(SOURCE_DIR):
-		s = os.path.join(SOURCE_DIR, item)
-		t = os.path.join(TARGET_DIR, item)
+	for item in os.listdir(source):
+		s = os.path.join(source, item)
+		t = os.path.join(target, item)
 
 		if os.path.isdir(s):
 			shutil.copytree(s, t)
@@ -32,7 +28,7 @@ def extract_title(markdown):
 		raise Exception
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
 	print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 	with open(from_path) as f1:
 		from_res = f1.read()
@@ -42,20 +38,20 @@ def generate_page(from_path, template_path, dest_path):
 	title = extract_title(from_res)
 	from_temp = from_temp.replace("{{ Title }}", title)
 	from_temp = from_temp.replace("{{ Content }}", html)
-	from_temp = from_temp.replace('href="/', 'href="{from_path}')
-	from_temp = from_temp.replace('src="/', 'src="{from_path}')
+	from_temp = from_temp.replace('href="/', f'href="{basepath}')
+	from_temp = from_temp.replace('src="/', f'src="{basepath}')
 	dest_file = Path(dest_path)
 	dest_file.parent.mkdir(parents=True, exist_ok=True)
 	with open(dest_path, "w", ) as f3:
 		f3.write(from_temp)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
 	for item in os.listdir(dir_path_content):
 		item_path = os.path.join(dir_path_content, item)
 		dest_path = os.path.join(dest_dir_path, item)
 		if os.path.isfile(item_path):
 			dest_path = Path(dest_path).with_suffix(".html")
-			generate_page(item_path, template_path, dest_path)
+			generate_page(item_path, template_path, dest_path, basepath)
 		elif os.path.isdir(item_path):
-			generate_pages_recursive(item_path, template_path, dest_path)
+			generate_pages_recursive(item_path, template_path, dest_path, basepath)
 	return
--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import sys
 from textnode import TextNode, TextType
 from copy_content import copy_content, extract_title, generate_page, generate_pages_recursive
 
-basepath = sys.argv[0] if sys.argv[0] else "/"
-
 def old_1():
 	new_node = TextNode("This is some anchor text", TextType('image'), "https://www.boot.dev")
 	print(new_node)
@@ -13,13 +11,17 @@ def old_2():
 
 def old_3():
 	copy_content()
-	generate_page("content/index.md", "template.html","public/index.html")
+	generate_page("content/index.md", "template.html", "public/index.html")
 
 def old_4():
-	copy_content()
-	generate_pages_recursive("content", "template.html","public")
+	copy_content("static", "public")
+	generate_pages_recursive("content", "template.html", "public")
 
 def main():
-	generate_pages_recursive(basepath, "template.html", "public")
+	basepath = "/"
+	if len(sys.argv) > 1:
+		basepath = sys.argv[1]
+	copy_content("static", "docs")
+	generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
